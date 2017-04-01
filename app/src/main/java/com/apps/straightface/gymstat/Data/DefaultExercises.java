938c6c1c699d008e3e5d3931c9bf947f4fd908e1
Jason Toms <jasonctoms@gmail.com>
2017-04-01 17:25:57 +0200

    Example of adding default exercises.

--- app/src/main/java/com/apps/straightface/gymstat/Data/DefaultExercises.java
+++ app/src/main/java/com/apps/straightface/gymstat/Data/DefaultExercises.java
@@ -1,10 +1,45 @@
 package com.apps.straightface.gymstat.Data;
 
+import android.content.Context;
+
+import com.apps.straightface.gymstat.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.realm.Realm;
+
 public class DefaultExercises
 {
-    public static void initializeDefaultExercises()
-    {
-        //TODO: add the default exercises here
+    private Realm mRealm;
 
+    public void initializeDefaultExercises(Context context)
+    {
+        mRealm = Realm.getDefaultInstance();
+
+        //TODO: add the default exercises here
+        List<String> defaultExercises = new ArrayList<>();
+
+        String barbellBenchName = context.getString(R.string.barbell_bench);
+        defaultExercises.add(barbellBenchName);
+
+
+        for(String name : defaultExercises)
+            addDefaultExercise(name);
+    }
+
+    private void addDefaultExercise(String name)
+    {
+        final String exerciseName = name;
+        mRealm.executeTransaction(new Realm.Transaction()
+        {
+            @Override
+            public void execute(Realm realm)
+            {
+                ExerciseObject e = realm.createObject(ExerciseObject.class);
+                e.setName(exerciseName);
+                //TODO: set image
+            }
+        });
     }
 }
--- app/src/main/java/com/apps/straightface/gymstat/GymStatApplication.java
+++ app/src/main/java/com/apps/straightface/gymstat/GymStatApplication.java
@@ -5,15 +5,20 @@ import android.app.Application;
 import com.apps.straightface.gymstat.Data.DefaultExercises;
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
+import io.realm.Realm;
+
 public class GymStatApplication extends Application
 {
     @Override
-    public void onCreate() {
+    public void onCreate()
+    {
         super.onCreate();
 
         AndroidThreeTen.init(this);
+        Realm.init(this);
 
         //TODO: check to make sure this hasn't been done yet
-        DefaultExercises.initializeDefaultExercises();
+        DefaultExercises defaultExercises = new DefaultExercises();
+        defaultExercises.initializeDefaultExercises(this);
     }
 }
