afd7d310805f2b79559a5f4c4e5201fb24d87105
Jason Toms <jasonctoms@gmail.com>
2017-04-17 10:01:48 +0200

    Added weekdays to routine card.

--- app/src/main/java/com/jorbital/gymstat/views/RoutinesAdapter.java
+++ app/src/main/java/com/jorbital/gymstat/views/RoutinesAdapter.java
@@ -1,7 +1,9 @@
 package com.jorbital.gymstat.views;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.annotation.Nullable;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,11 +15,14 @@ import com.jorbital.gymstat.GymStatStringConstants;
 import com.jorbital.gymstat.R;
 import com.jorbital.gymstat.data.Routine;
 import com.jorbital.gymstat.databinding.RoutinesListItemBinding;
+import com.jorbital.gymstat.utils.DayOfWeekRealm;
 
+import org.threeten.bp.DayOfWeek;
 import org.threeten.bp.Duration;
 import org.threeten.bp.LocalDateTime;
 
 import io.realm.OrderedRealmCollection;
+import io.realm.RealmList;
 import io.realm.RealmRecyclerViewAdapter;
 
 public class RoutinesAdapter extends RealmRecyclerViewAdapter<Routine, RoutinesAdapter.RoutineViewHolder>
@@ -70,6 +75,9 @@ public class RoutinesAdapter extends RealmRecyclerViewAdapter<Routine, RoutinesA
             int days = (int) Duration.between(then, now).toDays();
             b.lastPerformed.setText(String.format(itemView.getContext().getString(R.string.routines_last_performed), days));
 
+            RealmList<DayOfWeekRealm> weekdays = item.getDays();
+            setWeekdays(weekdays);
+
             b.routineExercisesRV.setHasFixedSize(true);
             b.routineExercisesRV.setLayoutManager(new LinearLayoutManager(itemView.getContext(),
                     LinearLayoutManager.VERTICAL, false));
@@ -77,6 +85,50 @@ public class RoutinesAdapter extends RealmRecyclerViewAdapter<Routine, RoutinesA
             b.routineExercisesRV.setAdapter(new RoutineExerciseListAdapter(item.getExercises(), true));
 
             b.executePendingBindings();
+        }
+
+        private void setWeekdays(RealmList<DayOfWeekRealm> weekdays)
+        {
+            for(DayOfWeekRealm day : weekdays)
+            {
+                int accent = ResourcesCompat.getColor(itemView.getResources(), R.color.colorAccent, null);
+
+                if(day.getEnum() == DayOfWeek.MONDAY)
+                {
+                    b.routineMonday.setTextColor(accent);
+                    b.routineMonday.setTypeface(null, Typeface.BOLD);
+                }
+                else if(day.getEnum() == DayOfWeek.TUESDAY)
+                {
+                    b.routineTuesday.setTextColor(accent);
+                    b.routineTuesday.setTypeface(null, Typeface.BOLD);
+                }
+                else if(day.getEnum() == DayOfWeek.WEDNESDAY)
+                {
+                    b.routineWednesday.setTextColor(accent);
+                    b.routineWednesday.setTypeface(null, Typeface.BOLD);
+                }
+                else if(day.getEnum() == DayOfWeek.THURSDAY)
+                {
+                    b.routineThursday.setTextColor(accent);
+                    b.routineThursday.setTypeface(null, Typeface.BOLD);
+                }
+                else if(day.getEnum() == DayOfWeek.FRIDAY)
+                {
+                    b.routineFriday.setTextColor(accent);
+                    b.routineFriday.setTypeface(null, Typeface.BOLD);
+                }
+                else if(day.getEnum() == DayOfWeek.SATURDAY)
+                {
+                    b.routineSaturday.setTextColor(accent);
+                    b.routineSaturday.setTypeface(null, Typeface.BOLD);
+                }
+                else if(day.getEnum() == DayOfWeek.SUNDAY)
+                {
+                    b.routineSunday.setTextColor(accent);
+                    b.routineSunday.setTypeface(null, Typeface.BOLD);
+                }
+            }
         }
 
         public void itemClicked(View v)
